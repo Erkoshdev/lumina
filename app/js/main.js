811b3d9c5eb1d87b78f80a7928f4ebc3198b956d
Erkebulan Safiolla <erkebulan91@gmail.com>
2022-05-23 01:47:43 +0600

    Signed-off-by: Erkebulan Safiolla <erkebulan91@gmail.com>

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+//Открыть меню на tablets
+$('.show-header-nav').click(function () {
+  $(this).toggleClass('active');
+  if ($(this).hasClass('active')) {
+    $('.header-nav').addClass('show')
+  } else {
+    $('.header-nav').removeClass('show')
+  }
+})
+
+$(document).ready(function () {
+  //настройка меню на mobile
+  if ($(window).width() < 767) {
+    let height = $('header').height();
+    let menuHeight = $(window).height() - height;
+    $('.mobile-menu').css({
+      'top': height,
+      'height': menuHeight
+    })
+  }
+
+  //добавление боковых отступов
+  if ($(window).width() > 991) {
+    $('.tide').addClass('container-cut')
+  }
+
+  //динамичная позиция main-slider nav-btn
+  let leftDistance = $('.slider-title h2').position().left + 5;
+  $('.slider-nav-btn').css('left', leftDistance);
+
+})
+
+//Открыть меню на mobile
+$('.show-mobile-menu').click(function () {
+  $(this).toggleClass('active');
+  if ($(this).hasClass('active')) {
+    $('.mobile-menu').addClass('show');
+    $('body').addClass('scroll-locked')
+  } else {
+    $('.mobile-menu').removeClass('show')
+    $('body').removeClass('scroll-locked')
+  }
+})
